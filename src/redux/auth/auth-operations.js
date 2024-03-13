7b345d670f8d2960422e71c15561f584a955a2cb
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2024-03-13 09:28:37 +0100

    3

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -4,12 +4,10 @@ import * as api from '../../shared/servises/auth-api';
 
 export const signup = createAsyncThunk('auth/signup', async (data, { rejectWithValue }) => {
   try {
-    // const { data: result } = await api.signup(data);
     const result = await api.signup(data);
     return result;
   } catch ({ response }) {
     return rejectWithValue(response.data.message);
-    // return rejectWithValue(response);
   }
 });
 
@@ -19,7 +17,6 @@ export const login = createAsyncThunk('auth/login', async (data, { rejectWithVal
     return result;
   } catch ({ response }) {
     return rejectWithValue(response.data.message);
-    // return rejectWithValue(response);
   }
 });
 
@@ -29,7 +26,6 @@ export const logout = createAsyncThunk('auth/logout', async (_, { rejectWithValu
     return data;
   } catch ({ response }) {
     return rejectWithValue(response.data.message);
-    // return rejectWithValue(response);
   }
 });
 
@@ -53,51 +49,3 @@ export const current = createAsyncThunk(
     },
   }
 );
-// export const signup = createAsyncThunk('auth/signup', async (data, { rejectWithValue }) => {
-//   try {
-//     // const { data: result } = await api.signup(data);
-//     const result = await api.signup(data);
-//     return result;
-//   } catch ({ response }) {
-//     return rejectWithValue(response);
-//   }
-// });
-
-// export const login = createAsyncThunk('auth/login', async (data, { rejectWithValue }) => {
-//   try {
-//     const result = await api.login(data);
-//     return result;
-//   } catch ({ response }) {
-//     return rejectWithValue(response);
-//   }
-// });
-
-// export const logout = createAsyncThunk('auth/logout', async (_, { rejectWithValue }) => {
-//   try {
-//     const data = await api.logout();
-//     return data;
-//   } catch ({ response }) {
-//     return rejectWithValue(response);
-//   }
-// });
-
-// export const current = createAsyncThunk(
-//   'auth/current',
-//   async (_, { rejectWithValue, getState }) => {
-//     try {
-//       const { auth } = getState();
-//       const data = await api.getCurrent(auth.token);
-//       return data;
-//     } catch ({ response }) {
-//       return rejectWithValue(response);
-//     }
-//   },
-//   {
-//     condition: (_, { getState }) => {
-//       const { auth } = getState();
-//       if (!auth.token) {
-//         return false;
-//       }
-//     },
-//   }
-// );
